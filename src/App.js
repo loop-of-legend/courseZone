7fd15b6e47052f5ea8574444dd6950181d0e92dc
semrin9 <aboodsemrin@hotmail.com>
2023-10-09 14:09:10 +0300

    Adding navbar & 1/2 of the sidebar :)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
+import {Home, SingleCourse, Cart, Courses} from "./pages";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-      <h2>Goodbye, World!</h2>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path = "/" element = {<Home />} />
+        <Route path = "/courses/:id" element = {<SingleCourse />} />
+        <Route path = "/category/:category" element = {<Courses />} />
+        <Route path = "/cart" element = {<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
